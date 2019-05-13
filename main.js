e60703bcffc2f406a6d047c58074a48842aa6507
TeoBotta <teombotta@gmail.com>
2019-05-13 11:48:52 -0300

    Subo código de las primeros dos módulos del tutorial oficial

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        product:'Socks',
+        description: 'Something that may o may not goes in your foot',
+        image: 'assets/vmSocks-green.jpg',
+        url: 'https://github.com/TeoBotta',
+        inventory: 20,
+        inStock: true
+    }
+})
