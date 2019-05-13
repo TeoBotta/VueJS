5d22e7804248aba796205e23d68567555f9b4ecf
TeoBotta <teombotta@gmail.com>
2019-05-13 16:54:52 -0300

    Subo código del séptimo módulo del tutorial oficial

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
 var app = new Vue({
     el:'#app',
     data:{
-        product:'Fender Squier Stratocaster',
+        product:'Squier Stratocaster',
+        marca:'Fender',
         description: 'Cuerpo de caoba y mastil de arce',
-        image: 'assets/strato_burn.jpg',
-        url: 'https://github.com/TeoBotta',
+        selectedColor: 0,
+        url: 'https://github.com/TeoBotta/VueJS',
         inventory: 20,
-        inStock: true,
+        /* inStock: true, */
         details: ["Tres pastillas de bobina simple con selector de cinco posiciones", "Puente con palanca de vibrato estilo vintage synchronized", "Numero de Frets: 21"],
         colors:[
             {
                 colorID: 1,
                 colorName: "Degradado",
                 colorImagen: "assets/strato_burn.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/brown-sunburst.png)"
+                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/brown-sunburst.png)",
+                colorCantidad:10
             },
             {
                 colorID: 2,
                 colorName: "Degradado 2",
                 colorImagen: "assets/strato_2color.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/2-color-sunburst.png)"
+                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/2-color-sunburst.png)",
+                colorCantidad:10
             },
             {
                 colorID: 3,
                 colorName: "Negro",
                 colorImagen: "assets/strato_negra.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/black.png)"
+                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/black.png)",
+                colorCantidad:0
             },
             {
                 colorID: 4,
                 colorName: "Rojo",
                 colorImagen: "assets/strato_roja.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10002/race-red.png)"
+                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10002/race-red.png)",
+                colorCantidad:10
             },
             {
                 colorID: 5,
                 colorName: "Celeste",
                 colorImagen: "assets/strato_surfgreen.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10003/surf-green.png)"
+                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10003/surf-green.png)",
+                colorCantidad:0
             }
         ],
         cart: 0
@@ -49,8 +55,20 @@ var app = new Vue({
         restarDelCarro () {
             this.cart -= 1
         },
-        cambiarImagen: function (colorImagen) {
-            this.image = colorImagen
+        cambiarImagen: function (index) {
+            this.selectedColor = index
+            console.log(index)
+        }
+    },
+    computed: {
+        prodtitle() {
+            return this.marca + ' ' +this.product
+        },
+        image() {
+            return this.colors[this.selectedColor].colorImagen
+        },
+        inStock() {
+            return this.colors[this.selectedColor].colorCantidad
         }
     }
 })
