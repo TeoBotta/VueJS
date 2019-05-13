053d86fbd6796381456dd81a0befada0a284faef
TeoBotta <teombotta@gmail.com>
2019-05-13 13:13:06 -0300

    Subo código del tercer y cuarto módulo del tutorial oficial

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 var app = new Vue({
     el:'#app',
     data:{
-        product:'Socks',
-        description: 'Something that may o may not goes in your foot',
-        image: 'assets/vmSocks-green.jpg',
+        product:'Fender Squier Stratocaster',
+        description: 'Cuerpo de caoba y mastil de arce',
+        image: 'assets/strato_burn.jpg',
         url: 'https://github.com/TeoBotta',
         inventory: 20,
-        inStock: true
+        inStock: true,
+        details: ["Tres pastillas de bobina simple con selector de cinco posiciones", "Puente con palanca de vibrato estilo vintage synchronized", "Numero de Frets: 21"],
+        colors:[
+            {
+                colorID: 1,
+                colorName: "Degradado"
+            },
+            {
+                colorID: 2,
+                colorName: "Degradado 2"
+            },
+            {
+                colorID: 3,
+                colorName: "Negro"
+            },
+            {
+                colorID: 4,
+                colorName: "Rojo"
+            },
+            {
+                colorID: 5,
+                colorName: "Celeste"
+            }
+        ]
     }
 })
