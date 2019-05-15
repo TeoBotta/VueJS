8a5d00089f7f4a2b2300b279d453b74c4c34492a
TeoBotta <teombotta@gmail.com>
2019-05-15 11:59:04 -0300

    Subo código del octavo módulo del tutorial oficial

--- main.js
+++ main.js
@@ -1,52 +1,103 @@
-var app = new Vue({
-    el:'#app',
-    data:{
-        product:'Squier Stratocaster',
-        marca:'Fender',
-        description: 'Cuerpo de caoba y mastil de arce',
-        selectedColor: 0,
-        url: 'https://github.com/TeoBotta/VueJS',
-        inventory: 20,
-        /* inStock: true, */
-        details: ["Tres pastillas de bobina simple con selector de cinco posiciones", "Puente con palanca de vibrato estilo vintage synchronized", "Numero de Frets: 21"],
-        colors:[
-            {
-                colorID: 1,
-                colorName: "Degradado",
-                colorImagen: "assets/strato_burn.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/brown-sunburst.png)",
-                colorCantidad:10
-            },
-            {
-                colorID: 2,
-                colorName: "Degradado 2",
-                colorImagen: "assets/strato_2color.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/2-color-sunburst.png)",
-                colorCantidad:10
-            },
-            {
-                colorID: 3,
-                colorName: "Negro",
-                colorImagen: "assets/strato_negra.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/black.png)",
-                colorCantidad:0
-            },
-            {
-                colorID: 4,
-                colorName: "Rojo",
-                colorImagen: "assets/strato_roja.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10002/race-red.png)",
-                colorCantidad:10
-            },
-            {
-                colorID: 5,
-                colorName: "Celeste",
-                colorImagen: "assets/strato_surfgreen.jpg",
-                colorBox: "url(https://www.fmicassets.com/Damroot/Original/10003/surf-green.png)",
-                colorCantidad:0
-            }
-        ],
-        cart: 0
+Vue.component('caracteristicas',{
+    template:`
+    <div>
+    <p>Caracteristicas:</p>
+    <ul>
+        <li v-for="detail in details">{{detail}}</li>
+    </ul>
+    </div>
+    `,
+    data() {
+        return {
+            details: ["Tres pastillas de bobina simple con selector de cinco posiciones", "Puente con palanca de vibrato estilo vintage synchronized", "Numero de Frets: 21"]
+        }
+    }
+})
+
+Vue.component('product',{
+    template:`
+    <div class="product">
+        <div class="product-image">
+            <img v-bind:src="image">
+        </div>
+        <div class="product-info">
+            <h1>{{ prodtitle }}</h1>
+            <h3>{{ description }}</h3>
+            <a v-bind:href="url" target="_blank">Enlace a repositorio de la pagina</a>
+            
+<!--        COMENTADO POR INNECESIDAD DE USO, PERO PARA RECORDAR LA SINTAXIS
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost out of stock</p>
+            <p v-else>Out of Stock</p>
+ -->
+            <p v-if="inStock">En Stock ({{inStock}})</p>
+            <p v-else>Sin Stock</p>
+<!--        CARACTERISTICAS COMENTADAS DADO QUE PASARON A UN COMPONENTE
+            <p>Caracteristicas:</p>
+            <ul>
+                <li v-for="detail in details">{{detail}}</li>
+            </ul>
+-->
+            <caracteristicas></caracteristicas>
+            <p>Variantes de color:</p>
+            <div v-for="(color,index) in colors" :key ="color.colorID" class="color-box" :style="{backgroundImage: color.colorBox}" @click="cambiarImagen(index)">
+                <!-- <p @mouseover="cambiarImagen(color.colorImagen)">{{color.colorName}}</p> -->
+            </div>
+        </div>
+        <button v-on:click="agregarAlCarro" :disabled="!inStock" :class="{disabledButton: !inStock}">+</button>
+        <button @click="restarDelCarro">-</button>
+        <div class="cart">
+            <p>Carro({{cart}})</p>
+        </div>
+    </div>
+    `,
+    data(){
+        return {
+            product:'Squier Stratocaster',
+            marca:'Fender',
+            description: 'Cuerpo de caoba y mastil de arce',
+            selectedColor: 0,
+            url: 'https://github.com/TeoBotta/VueJS',
+            inventory: 20,
+            colors:[
+                {
+                    colorID: 1,
+                    colorName: "Degradado",
+                    colorImagen: "assets/strato_burn.jpg",
+                    colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/brown-sunburst.png)",
+                    colorCantidad:10
+                },
+                {
+                    colorID: 2,
+                    colorName: "Degradado 2",
+                    colorImagen: "assets/strato_2color.jpg",
+                    colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/2-color-sunburst.png)",
+                    colorCantidad:7
+                },
+                {
+                    colorID: 3,
+                    colorName: "Negro",
+                    colorImagen: "assets/strato_negra.jpg",
+                    colorBox: "url(https://www.fmicassets.com/Damroot/Original/10001/black.png)",
+                    colorCantidad:0
+                },
+                {
+                    colorID: 4,
+                    colorName: "Rojo",
+                    colorImagen: "assets/strato_roja.jpg",
+                    colorBox: "url(https://www.fmicassets.com/Damroot/Original/10002/race-red.png)",
+                    colorCantidad:10
+                },
+                {
+                    colorID: 5,
+                    colorName: "Celeste",
+                    colorImagen: "assets/strato_surfgreen.jpg",
+                    colorBox: "url(https://www.fmicassets.com/Damroot/Original/10003/surf-green.png)",
+                    colorCantidad:0
+                }
+            ],
+            cart: 0
+        }
     },
     methods:{
         agregarAlCarro: function () {
@@ -71,4 +122,9 @@ var app = new Vue({
             return this.colors[this.selectedColor].colorCantidad
         }
     }
+
+})
+
+var app = new Vue({
+    el:'#app'
 })
